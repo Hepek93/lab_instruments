873c2793b0ffaecb381daff7935702f26a75724e
Hepek93 <n.djole.93@gmail.com>
2023-07-26 20:09:20 +0200

    Snimljeni podaci

--- osc_test.py
+++ osc_test.py
@@ -4,11 +4,14 @@ from time import sleep
 osc = Oscilloscope()
 
 osc.rigol.run()
-sleep(20)
+sleep(10)
 #osc.get_info(1)
-
+osc.rigol.set_memory_depth(300_000)
+osc.rigol.single()
+sleep(20)
 #voltage, time = osc.convert_data_to_v_t(osc.get_screen_data('CHAN1'))
 
 voltage1, time = osc.convert_data_to_v_t(osc.get_memory_data('CHAN1'))
 voltage2, time = osc.convert_data_to_v_t(osc.get_memory_data('CHAN2'))
-osc.write_to_csv('Rigol_snimci/230726/memorija_testera_1_polozaj_1s.csv', time, [voltage1, voltage2])
+voltage3, time = osc.convert_data_to_v_t(osc.get_memory_data('CHAN3'))
+osc.write_to_csv('Rigol_snimci/230726/memorija_rampa_1_polozaj_1s.csv', time, [voltage1, voltage2, voltage3])
